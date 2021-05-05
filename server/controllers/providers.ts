8711a87c1430ce9132d82a7b8d6b70f814ea32e9
David <david17895@gmail.com>
2021-05-05 14:40:32 +0300

    changed promise to async-await in providers

--- server/controllers/providers.ts
+++ server/controllers/providers.ts
@@ -1,90 +1,105 @@
-import uniqid from 'uniqid'
-import EmailProvider from '../models/email-provider'
-import { setSecret } from '../../helpers/secrets-management'
+import uniqid from "uniqid";
+import EmailProvider from "../models/email-provider";
+import { setSecret } from "../../helpers/secrets-management";
 
-export function createProvider(req, res) {
-  const body = req.body || {}
+export async function createProvider(req, res) {
+  const body = req.body || {};
   const provider = new EmailProvider({
     tenant: req.headers.tenant,
     name: body.name,
     kind: body.kind,
     metadata: body.metadata,
     access: body.access || { predefinedPublic: false },
-    authentication: uniqid()
-  })
+    authentication: uniqid(),
+  });
 
-  return setSecret(provider.tenant, provider.authentication, body.authentication)
-    .then(() => provider.save())
-    .then((provider) => {
-      res.status(200).json({
-        _id: provider._id,
-        name: provider.name,
-        kind: provider.kind,
-        metadata: provider.metadata
-      }).end()
-    })
-    .catch((err) => {
-      res.status(500).json({ message: 'email provider creation failed' }).end()
-    })
+  try {
+    await setSecret(provider.tenant, provider.authentication, body.authentication);
+
+    await provider.save();
+
+    return res.status(200).json({
+      _id: provider._id,
+      name: provider.name,
+      kind: provider.kind,
+      metadata: provider.metadata,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Email provider creation failed" });
+  }
 }
 
-export function getProvidersList(req, res) {
-  return EmailProvider.find({ tenant: req.headers.tenant })
-    .select('kind name metadata')
-    .lean()
-    .then(list => {
-      if (!list) {
-        return Promise.reject(null)
-      }
-      res.status(200).json(list).end()
-    })
-    .catch(() => {
-      res.status(500).json({ message: 'error loading email providers list' }).end()
-    })
+export async function getProvidersList(req, res) {
+  try {
+    const emailProviders = await EmailProvider.find({ tenant: req.headers.tenant }).select("kind name metadata").lean();
+
+    if (!emailProviders) {
+      return res.status(404).json({ message: "Email providers list not found" });
+    }
+
+    res.status(200).json(emailProviders);
+  } catch (error) {
+    return res.status(500).json({ message: "Error loading email providers list" });
+  }
 }
 
-export function removeProvider(req, res) {
-  req.provider.remove()
-    .then(() => res.status(200).json({}).end())
-    .catch(() => res.status(500).json({ message: 'failed to remove email provider' }).end())
+export async function removeProvider(req, res) {
+  try {
+    await req.provider.remove();
+
+    return res.status(500).json({});
+  } catch (error) {
+    res.status(500).json({ message: "Failed to remove email provider" });
+  }
 }
 
-export function updateProvider(req, res) {
-  const body = req.body || {}
-  let promise = Promise.resolve()
+export async function updateProvider(req, res) {
+  const body = req.body || {};
+  let newSecretSetter;
+
   if (body.name && body.name !== req.provider.name) {
-    req.provider.name = body.name
+    req.provider.name = body.name;
   }
   if ((body.kind && body.kind !== req.provider.kind) || body.authentication) {
-    req.provider.kind = body.kind || req.provider.kind
-    promise = setSecret(req.provider.tenant, req.provider.authentication, body.authentication)
+    req.provider.kind = body.kind || req.provider.kind;
+    newSecretSetter = newSecretSetter(req.provider.tenant, req.provider.authentication, body.authentication);
   }
   if (body.metadata) {
-    req.provider.metadata = body.metadata
+    req.provider.metadata = body.metadata;
   }
   if (body.access) {
-    req.provider.access = body.access
+    req.provider.access = body.access;
   }
-  promise
-    .then(() => req.provider.save())
-    .then(() => res.status(200).json({
-      name: req.provider.name,
-      kind: req.provider.kind,
-      metadata: req.provider.metadata
-    }).end())
-    .catch(() => res.status(500).json({ message: 'failed to update email provider' }).end())
+
+  try {
+    if (newSecretSetter) {
+      await newSecretSetter();
+    }
+
+    const updatedProvider = await req.provider.save();
+
+    return res.status(200).json({
+      name: updatedProvider.name,
+      kind: updatedProvider.kind,
+      metadata: updatedProvider.metadata,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Failed to update email provider" }).end();
+  }
 }
 
-export function getProviderById(req, res, next) {
-  return EmailProvider.findOne({ _id: req.params.providerId, tenant: req.headers.tenant })
-    .then(provider => {
-      req.provider = provider
-      next()
-    })
-    .catch(() => res.status(500).json({ message: 'could not find email provider' }).end())
+export async function getProviderById(req, res, next) {
+  const emailProvider = await EmailProvider.findOne({ _id: req.params.providerId, tenant: req.headers.tenant });
+
+  if (!emailProvider) {
+    return res.status(404).json({ message: `Email provider with the id of ${req.params.providerId} was not found` });
+  }
+
+  req.provider = emailProvider;
+  next();
 }
 
 export function getProvider(req, res) {
-  const { _id, name, kind, metadata } = req.provider
-  res.status(200).json({ _id, name, kind, metadata }).end()
+  const { _id, name, kind, metadata } = req.provider;
+  return res.status(200).json({ _id, name, kind, metadata });
 }
